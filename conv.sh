3d9f1c31e847d2400a8aeb867aeab56fa9c89fc1
Pauli Virtanen <pav@iki.fi>
2010-07-31 19:58:52 +0200

    Graft merge points

--- conv.sh
+++ conv.sh
@@ -4,13 +4,20 @@ cd /home/pauli/wrk/external/numpy2git
 rm -rf numpy f2py-research vendor
 
 # Convert
-svn-all-fast-export --identity-map authors.map --rules numpy.rules $PWD/../numpy-svn
+svn-all-fast-export \
+    --identity-map authors.map \
+    --rules numpy.rules \
+    --commit-interval 1000 \
+    --add-metadata \
+    $PWD/../numpy-svn \
+2>&1 | tee numpy-export.log
 
 # Postprocess
-./postprocess.sh numpy
+./postprocess.sh numpy numpy.grafts
 
 # Some manual cleanup
 (cd numpy && git branch -D maintenance/1.1.x_5227)
+(cd numpy && git branch -D master_1460)
 (cd numpy && git tag -d v0.9.6.2236)
 
 # Finalize
--- postprocess.sh
+++ postprocess.sh
@@ -3,12 +3,27 @@ set -e
 
 REPO="$1"
 REPOBASE=`basename "$REPO"`
+GRAFTS="$2"
 LOG="$PWD/postprocess-$REPOBASE.log"
+
+if test -d "$REPO/.git"; then
+    REPO="$REPO/.git"
+fi
 
 if test ! -d "$REPO"; then
     echo "Repository $REPO not found!"
     exit 1
 fi
+
+if test ! -f "$GRAFTS"; then
+    echo "Graft file $GRAFTS not found!"
+    exit 1
+fi
+
+# how can getting an absolute path be this hard?
+pushd `dirname "$GRAFTS"` > /dev/null
+GRAFTS="`pwd`/`basename "$GRAFTS"`"
+popd > /dev/null
 
 cd "$REPO"
 date -R > "$LOG"
@@ -16,16 +31,63 @@ date -R > "$LOG"
 run() {
     echo "\$" "$@"
     echo "\$" "$@" >> "$LOG"
-    $@ 2>&1 | tee -a "$LOG"
+    "$@" 2>&1 | tee -a "$LOG"
 }
 
-# 1) Remove crud branches
+msg() {
+    echo "$@" 2>&1 | tee -a "$LOG"
+}
+
+
+# 1) Inject graft points for merges
+
+> info/grafts
+
+cat "$GRAFTS" | while read BASE BRANCH; do
+    if test -z "$BASE" -a -z "$BRANCH"; then
+	continue
+    fi
+    if expr "$BASE" : "^#.*" > /dev/null; then
+	continue
+    fi
+    if test -z "$BASE" -a -z "$BRANCH"; then
+	msg "Incomplete graft point <$BASE>:<$BRANCH>"
+    fi
+    BASE_GREP=`echo "$BASE" | sed -e 's@^\(.*\):\(.*\)$@svn path=/\1/; revision=\2$@'|sed -e 's/\./\\./g'`
+    BRANCH_GREP=`echo "$BRANCH" | sed -e 's@^\(.*\):\(.*\)$@svn path=/\1/; revision=\2$@'|sed -e 's/\./\\./g'`
+    BASE_HASH=`git log --grep="$BASE_GREP" --format=format:%H --all`
+    BRANCH_HASH=`git log --grep="$BRANCH_GREP" --format=format:%H --all`
+    if test -z "$BASE_HASH" -o -z "$BRANCH_HASH"; then
+	msg "ERROR: Graft point <$BASE>:<$BRANCH> not found: <$BASE_HASH>:<$BRANCH_HASH>"
+	exit 1
+    fi
+    BASE_PARENT_HASH=`git rev-parse $BASE_HASH^`
+    msg "Grafting $BRANCH -> $BASE"
+    #msg "    base: $BASE_HASH"
+    #msg "    parent: $BASE_PARENT_HASH"
+    #msg "    added parent: $BRANCH_HASH"
+    echo "$BASE_HASH $BASE_PARENT_HASH $BRANCH_HASH" >> info/grafts
+done
+
+if test "$?" != 0; then
+    echo "Failure!"
+    exit 1
+fi
+
+exit 0
+
+# 2) Strip SVN metadata, and make the grafts permanent
+
+run git filter-branch --msg-filter 'head -n-2' -- --all
+rm -f info/grafts
+
+# 3) Remove crud branches
 
 for crud in `git branch|grep "^  crud/"`; do
     run git branch -D "$crud"
 done
 
-# 2) Convert SVN tag branches to real tags
+# 4) Convert SVN tag branches to real tags
 
 for branch in `git branch|grep "^  svntags/"`; do
     tag=`echo -n "$branch"|sed -e 's@svntags/v*@@'|sed -e 's/_/./g'`
@@ -34,5 +96,5 @@ for branch in `git branch|grep "^  svntags/"`; do
     run git branch -D "$branch"
 done
 
-# 3) Collect garbage
+# 4) Collect garbage
 run git prune
